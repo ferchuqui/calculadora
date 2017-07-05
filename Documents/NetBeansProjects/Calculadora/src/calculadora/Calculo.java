3d86643413c68f6f09d4b5d4a131c7951123dcc5
ferchuqui <luisfernandofarji@hotmail.com>
2017-07-05 10:44:56 -0300

    Implementacion de la clase negocio

---
+++ Documents/NetBeansProjects/Calculadora/src/calculadora/Calculo.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author alumno
+ */
+public class Calculo {
+    private Integer num1;
+    private Integer num2;
+    private Integer num3;
+    private Integer rst;
+    public void suma(){
+        
+    }
+    public void resta(){
+        
+    }
+    public void multi(){
+        
+    }
+    public void div(){
+        
+    }
+
+    public void setNum1(Integer num1) {
+        this.num1 = num1;
+    }
+
+    public void setNum2(Integer num2) {
+        this.num2 = num2;
+    }
+
+    public void setNum3(Integer num3) {
+        this.num3 = num3;
+    }
+
+    public Double getRst() {
+        return rst;
+    }
+    
+}
